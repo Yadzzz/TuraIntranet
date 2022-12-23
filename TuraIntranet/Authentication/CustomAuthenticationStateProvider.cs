0c313ca1157b95d0c1ed0af7149017c2cb1a4f57
Yad <yadmarzan@gmail.com>
2022-12-23 11:45:27 +0100

    Add project files.

---
+++ TuraIntranet/Authentication/CustomAuthenticationStateProvider.cs
@@ -0,0 +1,65 @@
+﻿using Microsoft.AspNetCore.Components.Authorization;
+using Microsoft.AspNetCore.Components.Server.ProtectedBrowserStorage;
+using System.Security.Claims;
+
+namespace TuraIntranet.Authentication
+{
+    public class CustomAuthenticationStateProvider : AuthenticationStateProvider
+    {
+        private readonly ProtectedSessionStorage _sessionStorage;
+        private ClaimsPrincipal _anonymous = new ClaimsPrincipal(new ClaimsIdentity());
+
+        public CustomAuthenticationStateProvider(ProtectedSessionStorage sessionStorage)
+        {
+            this._sessionStorage = sessionStorage;
+        }
+
+        public override async Task<AuthenticationState> GetAuthenticationStateAsync()
+        {
+            try
+            {
+                var userSessionStorageResult = await this._sessionStorage.GetAsync<UserSession>("UserSession");
+                var userSession = userSessionStorageResult.Success ? userSessionStorageResult.Value : null;
+
+                if (userSession == null)
+                {
+                    return await Task.FromResult(new AuthenticationState(this._anonymous));
+                }
+
+                var claimsPrincipal = new ClaimsPrincipal(new ClaimsIdentity(new List<Claim>
+                {
+                    new Claim(ClaimTypes.Name, userSession.Username),
+                    new Claim(ClaimTypes.Role, userSession.Role)
+                }, "CustomAuth"));
+
+                return await Task.FromResult(new AuthenticationState(claimsPrincipal));
+            }
+            catch
+            {
+                return await Task.FromResult(new AuthenticationState(this._anonymous));
+            }
+        }
+
+        public async Task UpdateAuthenticationState(UserSession userSession)
+        {
+            ClaimsPrincipal claimsPrincipal;
+
+            if(userSession != null)
+            {
+                await this._sessionStorage.SetAsync("UserSession", userSession);
+                claimsPrincipal = new ClaimsPrincipal(new ClaimsIdentity(new List<Claim>
+                {
+                    new Claim(ClaimTypes.Name, userSession.Username),
+                    new Claim(ClaimTypes.Role, userSession.Role)
+                }));
+            }
+            else
+            {
+                await this._sessionStorage.DeleteAsync("UserSession");
+                claimsPrincipal = this._anonymous;
+            }
+
+            NotifyAuthenticationStateChanged(Task.FromResult(new AuthenticationState(claimsPrincipal)));
+        }
+    }
+}
---
+++ TuraIntranet/Authentication/UserAccountService.cs
@@ -0,0 +1,52 @@
+﻿using System.DirectoryServices.AccountManagement;
+using System.Reflection.Metadata.Ecma335;
+
+namespace TuraIntranet.Authentication
+{
+    public class UserAccountService
+    {
+        public UserAccountService()
+        {
+            
+        }
+
+        public bool GetByUsername(string username, string password, out UserAccount userAccount, out string error)
+        {
+            userAccount = null;
+            error = string.Empty;
+
+            PrincipalContext context = new PrincipalContext(ContextType.Domain, "192.168.1.110", null, username, password);
+            UserPrincipal user = new UserPrincipal(context);
+            user.SamAccountName = username;
+            PrincipalSearcher searcher = new PrincipalSearcher(user);
+            var userPrincipal = searcher.FindOne();
+            var userGroups = new List<string>();
+
+            if (userPrincipal != null)
+            {
+                userGroups = userPrincipal.GetGroups(context).Select(x => x.Name).ToList();
+
+                if (!userGroups.Contains("Tura Intranet Users"))
+                {
+                    error = "Permission Denied for User";
+                    return false;
+                }
+                else
+                {
+                    userAccount = new UserAccount()
+                    {
+                        Username = username,
+                        Role = "User"
+                    };
+
+                    return true;
+                }
+            }
+            else
+            {
+                error = "Invalid Credentials";
+                return false;
+            }
+        }
+    }
+}
